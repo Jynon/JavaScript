97e1ec889e1ab65b865d16949933af40ee704820
Jynon <112575666+Jynon@users.noreply.github.com>
2022-10-17 05:21:02 -0700

    Add files via upload

--- assets/main.js
+++ assets/main.js
@@ -2,75 +2,56 @@ const inputMin = document.querySelector("#minInput");
 const inputMax = document.querySelector("#maxInput");
 const btnsubmit = document.querySelector("#generate");
 const btnclear = document.querySelector("#clear");
+const btnrefresh = document.querySelector("#refresh");
+const btnfizz = document.querySelector("#fizz")
+const btnbuzz = document.querySelector("#buzz")
+const btnfizzbuzz = document.querySelector("#fizzbuzz")
+
+const mainWrap = document.querySelector('.main-wrap');
+const card = document.querySelector('.card');
+
+const fizz = document.getElementsByClassName('fizz');
+const buzz = document.getElementsByClassName('buzz');
+const fizzbuzz = document.getElementsByClassName('fizzbuzz');
+const allnum  = document.getElementsByClassName('num')
 
 window.addEventListener('load', (event) => {
-    let mainWrap = document.querySelector('.main-wrap')
-
-
-    //inputMin.value = 1;
-    //inputMax.value = 100;
-
-    for (let index = 1; index <= 100; index++) {
-        let div = document.createElement('div');
-        let p = document.createElement('p');
-        div.classList.add('card');
-        div.appendChild(p);
-        mainWrap.appendChild(div)
-        p.innerHTML = index
-        if ((index % 5 == 0) && (index % 3 == 0)) {
-            console.log ('modulus 5 and 3 is 0')
-            div.classList.add('fizzbuzz')
-            p.innerHTML = index + "=FIZZBUZZ"}
-        else if (index % 5 == 0) {
-            console.log ('modulus 5 is 0')
-            div.classList.add('buzz')
-            p.innerHTML = index + "=BUZZ"}
-        else if (index % 3 == 0) {
-            console.log ('modulus 3 is 0')
-            div.classList.add('fizz')
-            p.innerHTML = index + "=FIZZ"}
-        else if ((index % 5 !== 0) && (index % 3 !== 0)) {
-            console.log(index)
-            div.classList.add('num')}
-            div.id = "myid" + index;
-    }
+    create();
 })
 
-//btnsubmit.addEventListener('click', submitElements)
-//btnclear.addEventListener('click', clearElements)
-
 btnsubmit.onclick = function() {
-        deleteAll()
-        create();
+    deleteAll()
+    create();
 }
     
 btnclear.onclick = function() {
-        deleteAll()
+    deleteAll()
 }
-    
-$(document).ready(function(){ $("fizz").click(function(){
-        $(".num, .buzz, .fizzbuzz").toggle();
-        });
-});
-$(document).ready(function(){ $("buzz").click(function(){
-        $(".num, .fizz, .fizzbuzz").toggle();
-        });
-});
-$(document).ready(function(){ $("fizzbuzz").click(function(){
-        $(".num, .buzz, .fizz").toggle();
-        });
-});
+
+btnrefresh.onclick = function() {
+    deleteAll()
+    inputMin.value = 1;
+    inputMax.value = 100;
+    create();
+}
+btnfizz.onclick = function() {
+    hide()
+    toggleClass(".fizz")
+}
+btnbuzz.onclick = function() {
+    hide()
+    toggleClass(".buzz")
+}
+btnfizzbuzz.onclick = function() {
+    hide()
+    toggleClass(".fizzbuzz")
+}
 
 function create() {
-        let mainWrap = document.querySelector('.main-wrap')
 
+    const min = parseInt(inputMin.value)
+    const max = parseInt(inputMax.value)
 
-    //inputMin.value = 1;
-    //inputMax.value = 100;
-console.log(inputMin.value)
-console.log(inputMax.value)
-const min = inputMin.value
-const max = inputMax.value
     for (let index = min; index <= max; index++) {
         let div = document.createElement('div');
         let p = document.createElement('p');
@@ -78,28 +59,47 @@ const max = inputMax.value
         div.appendChild(p);
         mainWrap.appendChild(div)
         p.innerHTML = index
-        if ((index % 5 == 0) && (index % 3 == 0)) {
-            console.log ('modulus 5 and 3 is 0')
+        if (index < min || index > max) {
+            div.style = "display: none";
+        }
+        else if ((index % 5 == 0) && (index % 3 == 0)) {
             div.classList.add('fizzbuzz')
-            p.innerHTML = index + "=FIZZBUZZ"}
+            p.innerHTML = index + "=FIZZBUZZ"
+        }
         else if (index % 5 == 0) {
-            console.log ('modulus 5 is 0')
             div.classList.add('buzz')
-            p.innerHTML = index + "=BUZZ"}
+            p.innerHTML = index + "=BUZZ"
+        }
         else if (index % 3 == 0) {
-            console.log ('modulus 3 is 0')
             div.classList.add('fizz')
-            p.innerHTML = index + "=FIZZ"}
+            p.innerHTML = index + "=FIZZ"
+        }
         else if ((index % 5 !== 0) && (index % 3 !== 0)) {
-            console.log(index)
-            div.classList.add('num')}
+            div.classList.add('num')
             div.id = "myid" + index;
+        }
     }
 }
 
 function deleteAll() {
-        const elements = document.querySelectorAll('.main-wrap > div')
-        elements.forEach(element => {
-                element.remove()
-        });
+    const elements = document.querySelectorAll('.main-wrap > div')
+    elements.forEach(element => {
+        element.remove()
+    });
+}
+
+function hide() {
+    const elements = document.querySelectorAll('.main-wrap > div')
+    elements.forEach(element => {
+        element.classList.add("hide")
+    });
+}
+
+function toggleClass(className) {
+    const elements = document.querySelectorAll(className)
+    elements.forEach(element => {
+        if (element.classList.contains("hide")) {
+            element.classList.remove("hide")
+        }
+    })
 }
